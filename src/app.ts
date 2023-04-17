ac4bd042d0f11ab21a60b2cf40084f94c525854e
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-17 22:45:34 +0300

    заготовки роутов

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express, { json, Request, Response } from "express";
 
 const { PORT = 3000, MONGO_URL = "mongodb://localhost:27017/mestodb" } =
   process.env;
@@ -6,3 +6,12 @@ const { PORT = 3000, MONGO_URL = "mongodb://localhost:27017/mestodb" } =
 const app = express();
 
 app.use(json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(201).send('test');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Номер порта ${PORT}, номер базы Mongo ${MONGO_URL}`)
+})
