37f16da1f14449b42c9dacc4e515ff6b6cc008f1
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-17 22:54:06 +0300

    заглушки для роутера cards

--- src/routes/cardsRouter.ts
+++ src/routes/cardsRouter.ts
@@ -1,3 +1,11 @@
 import { Router } from 'express';
 
 const cardsRouter = Router();
+
+cardsRouter.get('/', () => {"получаем все карточки"});
+cardsRouter.post('/', () => {"создаем"});
+cardsRouter.put('/:cardId/likes', () => {"ставим лайк"});
+cardsRouter.delete('/:cardId/likes', () => {"убираем лайк"});
+cardsRouter.delete('/:cardId', () => {"удаляем карточку"});
+
+export default cardsRouter;
