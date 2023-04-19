b709c862e09497cb69107969353feb0f6c154c77
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-19 17:13:43 +0300

    схема для user

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,36 @@
 import mongoose from "mongoose";
+import { regExp } from "utils/constants";
 import { TUser } from "utils/types";
 
-
 const UserSchema = new mongoose.Schema<TUser>({
-  name:{},
-  about:{},
-  avatar:{},
-})
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+    validate: {
+      validator: (valid: string) => valid.length > 2 && valid.length < 30,
+      message: "Текст короче 2 символов или длиннее 30",
+    },
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 200,
+    required: true,
+    validate: {
+      validator: (v: string) => v.length > 2 && v.length < 200,
+      message: "Текст короче 2 символов или длиннее 200",
+    },
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v: string) => regExp.test(v),
+      message: "Некорректная ссылка",
+    },
+  },
+});
 
-export default mongoose.model<TUser>('User', UserSchema);
+export default mongoose.model<TUser>("User", UserSchema);
