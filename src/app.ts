102b4709b5971e010ca92c5ad02255c447d659d6
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-19 16:51:53 +0300

    функция для запуска и подключения к базе данных

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
 import express, { json, Request, Response } from "express";
+import path from 'path';
+import mongoose from 'mongoose';
 
-const { PORT = 3000, MONGO_URL = "mongodb://localhost:27017/mestodb" } =
+
+const { PORT = 4444, MONGODB_URL = "mongodb://localhost:27017/mestodb" } =
   process.env;
 
 const app = express();
 
 app.use(json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(201).send('test');
+app.get("/", (req: Request, res: Response) => {
+  res.status(201).send("test");
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Номер порта ${PORT}, номер базы Mongo ${MONGO_URL}`)
-})
+
+async function start() {
+  try {
+    mongoose.set('strictQuery', false); //для теста
+    await mongoose.connect(MONGODB_URL);
+    console.log('База данных подключена');
+    await app.listen(PORT);
+    console.log('Сервер запущен', PORT);
+  } catch (error) {
+    console.log('Ошибка сервера', error);
+  }
+}
+start()
