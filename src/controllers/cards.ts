8507cd0b5581b89582da48f95f6043989295a217
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-21 00:37:05 +0300

    создание карточки

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -15,3 +15,18 @@ export const getCards = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const createCard = async (req: Request, res: Response) => {
+  const { name, link } = req.body;
+  const owner = (req as IRequest).user?._id;
+  try {
+    if (!name || !link) {
+      return res.status(400).send("Проверьте данные для создания карточки");
+    }
+    const card = await Card.create({ name, link, owner });
+    return res.status(201).json({ data: card });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Не удалось создать карточку");
+  };
+  };
--- src/routes/cardsRouter.ts
+++ src/routes/cardsRouter.ts
@@ -1,10 +1,10 @@
-import { getCards } from '../controllers/cards';
+import { createCard, getCards } from '../controllers/cards';
 import { Router } from 'express';
 
 const cardsRouter = Router();
 
 cardsRouter.get('/', getCards);
-cardsRouter.post('/', () => {"создаем"});
+cardsRouter.post('/', createCard);
 cardsRouter.put('/:cardId/likes', () => {"ставим лайк"});
 cardsRouter.delete('/:cardId/likes', () => {"убираем лайк"});
 cardsRouter.delete('/:cardId', () => {"удаляем карточку"});
