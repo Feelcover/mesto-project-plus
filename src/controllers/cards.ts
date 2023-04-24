e9e81139b9f535b8f13fe2e6e32e6fda0beeeb15
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-24 19:20:07 +0300

    удаление карточки с проверкой

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -31,18 +31,18 @@ export const createCard = async (
       next(new BadRequestErr("Проверьте данные для создания карточки"));
     }
     const card = await Card.create({ name, link, owner });
-    return res.status(201).send({ statusCard:"created", data: card });
+    return res.status(201).send({ statusCard: "created", data: card });
   } catch (err) {
     next(new InternalServerErr("На сервере произошла ошибка"));
   }
 };
-export const getCardById = async (req:Request, res:Response, next:NextFunction) => {
+export const getCardById = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const card = await Card.findById(req.params.cardId);
     if (!card) {
       next(new NotFoundErr("Карточка не найдена"));
     }
-    return res.status(200).send({data:{card}})
+    return res.status(200).send({ data: { card } })
   } catch (err) {
     next(new InternalServerErr("На сервере произошла ошибка"));
   }
@@ -54,16 +54,17 @@ export const deleteCard = async (
   next: NextFunction
 ) => {
   const { cardId } = req.params;
-  const user = (req as IRequest).user?._id;
+  const userId = (req as IRequest).user?._id;
   try {
     const card = await Card.findByIdAndRemove(cardId);
     if (!card) {
       next(new NotFoundErr("Не удалось найти карточку"));
     }
-    if (card && card.owner.toString() !== user?.toString()) {
+    if (card && card.owner.toString() !== userId?.toString()) {
       next(new ForbiddenError("Нет прав для удаления карточки"))
+    } else {
+      return res.status(200).send({statusCard: "deleted", data: card });
     }
-    return res.status(200).send({ statusCard:"deleted", data: card });
   } catch (err) {
     next(new InternalServerErr("На сервере произошла ошибка"));
   }
