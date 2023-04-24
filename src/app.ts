d4f2804be646a990d92e3e2824237396b7b5cdbd
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-24 21:51:32 +0300

    подключение логгеров

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,34 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import routes from './routes/index';
-import { testUserId } from './utils/testUser';
-import { DB_URL, PORT } from './utils/constants';
-import authMiddleware from './middleware/authMiddleware';
-import helmet from 'helmet';
-import { errorHandler } from './errors/errHandler';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import routes from "./routes/index";
+import { testUserId } from "./utils/testUser";
+import { DB_URL, PORT } from "./utils/constants";
+import authMiddleware from "./middleware/authMiddleware";
+import helmet from "helmet";
+import { errorHandler } from "./errors/errHandler";
+import { errLogger, reqLogger } from "errors/loggers";
 
 const app = express();
 
 app.use(express.json());
-app.use(helmet())
+app.use(helmet());
+app.use(reqLogger);
+app.use(errLogger);
 // app.use(testUserId);
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(authMiddleware);
 app.use(routes);
-app.use(errorHandler)
+app.use(errorHandler);
 
 const start = async () => {
   try {
-    mongoose.set('strictQuery', false);
+    mongoose.set("strictQuery", false);
     await mongoose.connect(DB_URL);
-    console.log('База данных подключена', DB_URL);
+    console.log("База данных подключена", DB_URL);
     await app.listen(PORT);
-    console.log('Сервер запущен', PORT);
+    console.log("Сервер запущен", PORT);
   } catch (err) {
-    console.log('Ошибка подключения', err);
+    console.log("Ошибка подключения", err);
   }
 };
 
