db8e3644a5dfe036e264b9b1ae593da9e683115a
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-24 23:37:25 +0300

    фикс errHandler

--- src/errors/errHandler.ts
+++ src/errors/errHandler.ts
@@ -1,9 +1,8 @@
-import { InternalServerErr } from "./errors";
 import { NextFunction, Request, Response } from "express";
 
 export const errorHandler = (req:Request, res:Response, next:NextFunction) => {
   if(req.xhr) {
-    next(new InternalServerErr("На сервере произошла ошибка"))
+    res.status(500).send({ error: 'Произошла ошибка!' });
   }else{
     next();
   }
