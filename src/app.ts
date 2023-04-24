bdcae08f815c70ad64c9594735c50981df62d29a
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-24 23:30:57 +0300

    фикс доступа к регистрации и логину

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import helmet from "helmet";
 import { errorHandler } from "./errors/errHandler";
 import { errLogger, reqLogger } from "./errors/loggers";
 import { errors } from 'celebrate';
+import { createUserValidate, loginValidate } from "./validations/validationAuth";
+import { createUser, login } from "./controllers/users";
 
 const app = express();
 
@@ -15,6 +17,8 @@ app.use(helmet());
 app.use(reqLogger);
 app.use(errLogger);
 app.use(errors());
+app.post('/signup', createUserValidate, createUser);
+app.post('/signin', loginValidate, login);
 app.use(authMiddleware);
 app.use(routes);
 app.use(errorHandler);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Router } from 'express';
-import authRouter from './authRouter';
 import cardsRouter from './cardsRouter';
 import usersRoutes from './usersRouter';
 
@@ -7,7 +6,6 @@ const routes = Router();
 
 routes.use('/users', usersRoutes);
 routes.use('/cards', cardsRouter);
-routes.use('/', authRouter);
 routes.use((req: Request, res: Response) => res.status(404).send('Страница не найдена'));
 
 export default routes;
